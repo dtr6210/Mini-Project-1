1dfb5b6207c2960f69d01bf48463c89dcbd80b78
dtr6210 <rialsdean@gmail.com>
2023-11-30 22:05:03 -0600

    updated cards to include recipes

--- index.js
+++ index.js
@@ -11,40 +11,50 @@ document.addEventListener("DOMContentLoaded", function () {
       axios
         .get(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${category}`)
         .then((response) => {
-          console.log(response.data);
-          const meals = Array.isArray(response.data.meals) ? response.data.meals: [];
+          console.log(response.data.meals);
+          const meals = Array.isArray(response.data.meals) ? response.data.meals: [{}];
   
        
           meals.forEach((meal) => {
+//Fetch detailed information for each meal
+axios
+.get(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${meal.idMeal}`)
+.then((detailedResponse) => {
+    const detailedMeal = detailedResponse.data.meals[0];
+    console.log("Detailed Meal details:", detailedMeal);
+
+
+
             const card = document.createElement("div");
             card.className = "card";
             card.style = "width: 18rem";
   
             const cardImg = document.createElement("img");
             cardImg.className = "card-img-top";
-            cardImg.src = meal.strMealThumb;
-            cardImg.alt = meal.strMeal;
+            cardImg.src = detailedMeal.strMealThumb;
+            cardImg.alt = detailedMeal.strMeal;
   
             const cardBody = document.createElement("div");
             cardBody.className = "card-body";
   
             const cardTitle = document.createElement("h5");
             cardTitle.className = "card-title";
-            cardTitle.textContent = meal.strMeal;
+            cardTitle.textContent = detailedMeal.strMeal;
 
             const cardInstructions = document.createElement("p");
             cardInstructions.className = "card-text";
-            cardInstructions.textContent = meal.strInstructions;
+            cardInstructions.textContent = detailedMeal.strInstructions;
   
   
             cardBody.appendChild(cardTitle);
-            card.appendChild(cardInstructions);
+            cardBody.appendChild(cardInstructions);
             card.appendChild(cardImg);
             card.appendChild(cardBody);
   
             postContainer.appendChild(card);
           });
         });
+    });
     }
   
     //Event listener for the select element
